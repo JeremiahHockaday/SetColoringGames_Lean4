7843dc3b49348f1ffb659ebe81c38fe6c91501a6
jeremiah <jr325035@dal.ca>
2026-04-26 16:26:13 -0300

    + added public imports, fixes some bugs + migrated four definitions/lemmas   - sub_temp_left   - sub_temp_right   - subtype_set_im_equiv   - subtype_set_small

--- MyCGTProject/SmallNonempty.lean
+++ MyCGTProject/SmallNonempty.lean
@@ -6,13 +6,12 @@ Authors: Violeta Hernández Palacios
 module
 
 public import Mathlib.Logic.Small.Defs
+public import Mathlib.Logic.Small.Set
 
-import Mathlib.Logic.Small.Set
 import Mathlib.Logic.Relation
 import Mathlib.Order.SetNotation
 
 import Mathlib.Data.Fintype.Defs
-import Mathlib.Logic.Small.Set
 import Mathlib.Data.QPF.Univariate.Basic
 import Mathlib.Data.Set.Image
 import Mathlib.Data.Set.Basic
@@ -50,7 +49,59 @@ noncomputable def nonempty_range {α β} (f : α → β) (X : Set α) (hx : X.No
   use (f b) 
   use b  
 
+------------------
+def sub_temp_left {α : Type (u + 1)} (p : α → Prop) (X : Set (Subtype p)) : X→ Set.image Subtype.val X := fun x => ⟨x.val, by 
+    let ⟨⟨a,ha⟩,hy⟩ := x;
+    simp only [Set.mem_image, Subtype.exists, exists_and_right, exists_eq_right]
+    constructor
+    · exact hy
+    · exact ha 
+    ⟩
 
+def sub_temp_right {α : Type (u + 1)} (p : α → Prop) (X : Set (Subtype p)) : Set.image Subtype.val X → X := fun x => ⟨⟨x.val, by 
+    let ⟨a,⟨⟨b1,b2⟩,⟨hb1,hb2⟩⟩⟩ := x;
+    simp only at hb2
+    simp only [hb2] at b2
+    simp only
+    exact b2⟩, by
+      let ⟨a,⟨⟨b1,b2⟩,⟨hb1,hb2⟩⟩⟩ := x;
+      simp only at hb2
+      have hn : p a := by 
+        rw [hb2] at b2
+        exact b2
+      have hx : Subtype.mk b1 b2 = ⟨a,hn⟩:= by
+        ext
+        simp only
+        exact hb2;
+      simp [← hx, hb1]⟩
+
+def subtype_set_im_equiv {α : Type (u + 1)} (p : α → Prop) (X : Set (Subtype p)) : Equiv X (Set.image Subtype.val X) := 
+  ⟨sub_temp_left p X 
+  ,
+  sub_temp_right p X
+  , 
+  by
+  intro x
+  simp only [sub_temp_right,sub_temp_left]
+  , 
+  by
+  intro x
+  simp only [sub_temp_right, sub_temp_left, Subtype.coe_eta]
+  ⟩
+
+lemma subtype_set_small {α : Type (u + 1)} (p : α → Prop) (X : Set (Subtype p)) [Small.{u} X] : Small.{u} (Set.image Subtype.val X):= by 
+  let f := sub_temp_left p X;
+  have hg : Function.Injective (sub_temp_right p X) := by
+    intro b c hi
+    simp only [sub_temp_right,Subtype.mk.injEq] at hi
+    ext 
+    rw [hi]
+  exact small_of_injective (hg)
+
+
+
+
+-------------------
 variable {α : Type*} (r : α → α → Prop) [H : ∀ x, Small.{u} {y // r x y}]
 
 private def level [∀ x, Small.{u} {y // r x y}] (x : α) : ℕ → Set α
