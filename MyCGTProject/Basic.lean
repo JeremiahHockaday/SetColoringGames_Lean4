486dfed911e7f11b13e7a3d2cfa20a7ee264f7e6
jeremiah <jr325035@dal.ca>
2026-04-26 23:49:51 -0300

    + Progress : proved that Primordial.Subposition is well founded

--- MyCGTProject/Basic.lean
+++ MyCGTProject/Basic.lean
@@ -216,164 +216,159 @@ theorem ofSets_inj {A : Type} {s₁ s₂ t₁ t₂ : Set (Primordial A)} [Small 
 -- Because of the diffference between composite and atomic games, we must define subpositions very carefully.
 
 
--- /-- option x y : y is composite and x is in the left or right set of the game y. -/
--- def option {A : Type} : (Primordial.{u} A) → (Primordial.{u} A) → Prop := fun x y => ∃ h:y ∈ Comp.{u} A, x ∈ ⋃ p, (moves.{u} ⟨y,h⟩) p
+/-- option x y : y is composite and x is in the left or right set of the game y. -/
+def option {A : Type} : (Primordial.{u} A) → (Primordial.{u} A) → Prop := fun x y => ∃ h:y ∈ Comp.{u} A, x ∈ ⋃ p, (moves.{u} ⟨y,h⟩) p
 
--- /-- x is a left option of the game y -/
--- def LOption {A : Type} : (Primordial.{u} A) → (Primordial.{u} A) → Prop := fun x y => ∃ h:y ∈ Comp.{u} A, x ∈ (moves.{u} ⟨y,h⟩) left
+/-- x is a left option of the game y -/
+def LOption {A : Type} : (Primordial.{u} A) → (Primordial.{u} A) → Prop := fun x y => ∃ h:y ∈ Comp.{u} A, x ∈ (moves.{u} ⟨y,h⟩) left
 
--- /-- x is a right option of the game y -/
--- def ROption {A : Type} : (Primordial.{u} A) → (Primordial.{u} A) → Prop := fun x y => ∃ h:y ∈ Comp.{u} A, x ∈ (moves.{u} ⟨y,h⟩) right
+/-- x is a right option of the game y -/
+def ROption {A : Type} : (Primordial.{u} A) → (Primordial.{u} A) → Prop := fun x y => ∃ h:y ∈ Comp.{u} A, x ∈ (moves.{u} ⟨y,h⟩) right
 
--- /-- x is an option of y iff x is a left or right option of y. -/
--- theorem option_iff_lroption {x y : Primordial A} : option x y ↔ LOption x y ∨ ROption x y := by 
---   dsimp [option, LOption, ROption]
---   simp only [Set.mem_iUnion, Player.exists]
---   constructor
---   · intro ⟨h,hor⟩
---     cases hor with
---     | inl hl => 
---       left
---       use h
---     | inr hr => 
---       right
---       use h
---   · intro mpr
---     cases mpr with
---     | inl hl => 
---       let ⟨hl,hly⟩ := hl
---       use hl
---       left
---       exact hly
---     | inr hr => 
---       let ⟨hr,hry⟩ := hr
---       use hr
---       right
---       exact hry
+/-- x is an option of y iff x is a left or right option of y. -/
+theorem option_iff_lroption {x y : Primordial A} : option x y ↔ LOption x y ∨ ROption x y := by 
+  dsimp [option, LOption, ROption]
+  simp only [Set.mem_iUnion, Player.exists]
+  constructor
+  · intro ⟨h,hor⟩
+    cases hor with
+    | inl hl => 
+      left
+      use h
+    | inr hr => 
+      right
+      use h
+  · intro mpr
+    cases mpr with
+    | inl hl => 
+      let ⟨hl,hly⟩ := hl
+      use hl
+      left
+      exact hly
+    | inr hr => 
+      let ⟨hr,hry⟩ := hr
+      use hr
+      right
+      exact hry
 
 
--- /-- A proper subposition of a (composite) game y is any game reachable by a nonempty sequence of left and right moves. -/
--- def Subposition {A : Type} : (Primordial A) -> (Primordial A) -> Prop := Relation.TransGen option
+/-- A proper subposition of a (composite) game y is any game reachable by a nonempty sequence of left and right moves. -/
+def Subposition {A : Type} : (Primordial A) -> (Primordial A) -> Prop := Relation.TransGen option
 
--- theorem optionSubposition {A : Type} {x y : Primordial A} : option x y → Subposition x y := λ ho => by 
---   unfold Subposition
---   rw [Relation.transGen_iff]
---   left
---   exact ho
+theorem optionSubposition {A : Type} {x y : Primordial A} : option x y → Subposition x y := λ ho => by 
+  unfold Subposition
+  rw [Relation.transGen_iff]
+  left
+  exact ho
 
--- @[aesop unsafe apply 50%]
--- theorem Subposition.of_mem_moves {A : Type} {x : Primordial A} {y : Comp A} (h : x ∈ ⋃ p, (moves.{u} y) p) : Subposition x y.1 :=
---   Relation.TransGen.single (by 
---     unfold option
---     have h':y.1∈ Comp A := by 
---       simp
---     use h')
+@[aesop unsafe apply 50%]
+theorem Subposition.of_mem_moves {A : Type} {x : Primordial A} {y : Comp A} (h : x ∈ ⋃ p, (moves.{u} y) p) : Subposition x y.1 :=
+  Relation.TransGen.single (by 
+    unfold option
+    have h':y.1∈ Comp A := by 
+      simp
+    use h')
 
--- /-- transitivity of Subposition relation -/
--- theorem Subposition.trans {A : Type} {a b c : Primordial A} : Subposition a b → Subposition b c -> Subposition a c := Relation.TransGen.trans
+/-- transitivity of Subposition relation -/
+theorem Subposition.trans {A : Type} {a b c : Primordial A} : Subposition a b → Subposition b c -> Subposition a c := Relation.TransGen.trans
 
--- instance {A : Type} : IsTrans (Primordial A) Subposition := inferInstanceAs (IsTrans _ (Relation.TransGen _))
+instance {A : Type} : IsTrans (Primordial A) Subposition := inferInstanceAs (IsTrans _ (Relation.TransGen _))
   
 
--- instance comp.small_setOf_options {A : Type} : 
--- ∀ x : (Comp.{u} A),  Small.{u , u + 1} {y : Primordial A | y ∈ ⋃ p, (moves x) p} := 
---   λ x =>
---   have h1 : Small.{u} (⋃ p, (moves x) p) := by infer_instance;
---   let f : {y : Primordial A | y ∈ ⋃ p, (moves x) p} → ⋃ p, (moves x) p := λ x => x;
---   let g : ⋃ p, (moves x) p→{y : Primordial A | y ∈ ⋃ p, (moves x) p} := λ x => x;
---   have h3 : g∘f = id := by congr;
---   have h4 : Function.LeftInverse g f := by 
---     dsimp [Function.LeftInverse]
---     intro x
---     congr;
---   small_of_injective (Function.LeftInverse.injective h4)
+instance comp.small_setOf_options {A : Type} : 
+∀ x : (Comp.{u} A),  Small.{u , u + 1} {y : Primordial A | y ∈ ⋃ p, (moves x) p} := 
+  λ x =>
+  have h1 : Small.{u} (⋃ p, (moves x) p) := by infer_instance;
+  let f : {y : Primordial A | y ∈ ⋃ p, (moves x) p} → ⋃ p, (moves x) p := λ x => x;
+  let g : ⋃ p, (moves x) p→{y : Primordial A | y ∈ ⋃ p, (moves x) p} := λ x => x;
+  have h3 : g∘f = id := by congr;
+  have h4 : Function.LeftInverse g f := by 
+    dsimp [Function.LeftInverse]
+    intro x
+    congr;
+  small_of_injective (Function.LeftInverse.injective h4)
 
--- instance Atom_no_options {A : Type} (x : Primordial.{u} A) (h : Sum.isLeft (moves_or x)) : IsEmpty {y : Primordial A // y.option x}:=
--- have h1 := Sum.isRight_eq_false.mpr h;
---     have h2 : x ∉ Comp.{u} A  := by 
---       false_or_by_contra
---       have h2a : Sum.isRight (moves_or x) := by congr;
---       have h2b : true = false := by 
---         have h2c : x=x := rfl;
---         apply @congrArg _ _ x x ( Sum.isRight∘(@moves_or A)) at h2c 
---         dsimp [Function.comp] at h2c
---         nth_rewrite 1 [h2a] at h2c
---         rw [h1] at h2c
---         exact h2c
---       contradiction;
---     have h3 : ∀ y : {y : Primordial A | option y x}, False := by 
---       intro ⟨y,a⟩
---       apply Exists.nonempty at a;
---       apply (@nonempty_prop (x ∈ Comp A)).mp at a;
---       contradiction;
---     by exact (isEmpty_iff.mpr h3)
+instance Atom_no_options {A : Type} (x : Primordial.{u} A) (h : Sum.isLeft (moves_or x)) : IsEmpty {y : Primordial A // y.option x}:=
+have h1 := Sum.isRight_eq_false.mpr h;
+    have h2 : x ∉ Comp.{u} A  := by 
+      false_or_by_contra
+      have h2a : Sum.isRight (moves_or x) := by congr;
+      have h2b : true = false := by 
+        have h2c : x=x := rfl;
+        apply @congrArg _ _ x x ( Sum.isRight∘(@moves_or A)) at h2c 
+        dsimp [Function.comp] at h2c
+        nth_rewrite 1 [h2a] at h2c
+        rw [h1] at h2c
+        exact h2c
+      contradiction;
+    have h3 : ∀ y : {y : Primordial A | option y x}, False := by 
+      intro ⟨y,a⟩
+      apply Exists.nonempty at a;
+      apply (@nonempty_prop (x ∈ Comp A)).mp at a;
+      contradiction;
+    by exact (isEmpty_iff.mpr h3)
 
--- instance small_setOf_options {A : Type} : ∀ x : (Primordial.{u} A),  Small.{u , u + 1} {y : Primordial A // y.option x} := λ x => by 
---   dsimp [option]
---   cases h : Sum.isLeft (moves_or x) 
---   · have h1 : (moves_or x).isRight := Sum.isLeft_eq_false.mp h;
---     have h2 : x∈ Comp.{u} A := by congr;
---     let f : {y : Primordial A | option y x} → {y:Primordial A | y∈ ⋃ p, (moves ⟨x,h2⟩) p} := 
---       λ y => ⟨y.1, y.2.2⟩;
---     have h3 : Function.Injective f := by 
---       intro ⟨x,xh⟩ ⟨y,yh⟩ h'
---       unfold f at h'
---       congr
---       apply Subtype.ext_iff.mp at h'
---       exact h';
---     exact small_of_injective (h3);
---   · have h1 := Atom_no_options x h;
---     exact @small_isEmpty {y : Primordial A // y.option x} h1
+instance small_setOf_options {A : Type} : ∀ x : (Primordial.{u} A),  Small.{u , u + 1} {y : Primordial A // y.option x} := λ x => by 
+  dsimp [option]
+  cases h : Sum.isLeft (moves_or x) 
+  · have h1 : (moves_or x).isRight := Sum.isLeft_eq_false.mp h;
+    have h2 : x∈ Comp.{u} A := by congr;
+    let f : {y : Primordial A | option y x} → {y:Primordial A | y∈ ⋃ p, (moves ⟨x,h2⟩) p} := 
+      λ y => ⟨y.1, y.2.2⟩;
+    have h3 : Function.Injective f := by 
+      intro ⟨x,xh⟩ ⟨y,yh⟩ h'
+      unfold f at h'
+      congr
+      apply Subtype.ext_iff.mp at h'
+      exact h';
+    exact small_of_injective (h3);
+  · have h1 := Atom_no_options x h;
+    exact @small_isEmpty {y : Primordial A // y.option x} h1
 
--- instance small_setOf_subposition {A : Type} (x : Primordial.{u} A) : Small.{u} {y : Primordial A  | Subposition y x} :=
---   small_transGen' _ x 
+instance small_setOf_subposition {A : Type} (x : Primordial.{u} A) : Small.{u} {y : Primordial A  | Subposition y x} :=
+  small_transGen' _ x 
 
 -- -------------------------------------------------  
 
--- lemma tempName {α : Type (u + 1)} {q : α → Prop}
--- (st : Player → Set (Subtype q))
--- (hst : st ∈ {s: Player → Set (Subtype q)|∀ p, (Small.{u} (s p) ∧ (s p).Nonempty)}) : (λ p => Set.image Subtype.val (st p)) ∈ {s: Player→Set α| ∀ p, (Small.{u} (s p) ∧ (s p).Nonempty)} := by 
---   simp only [Player.forall, Set.mem_setOf_eq, Set.image_nonempty]
---   simp only [Player.forall, Set.mem_setOf_eq] at hst
---   let ⟨⟨a,b⟩,⟨c,d⟩⟩ := hst
---   constructor
---   constructor
---   · exact subtype_set_small (q) (st Player.left)
---   · exact b
---   constructor
---   · exact subtype_set_small (q) (st Player.right)
---   · exact d;
+lemma tempName {α : Type (u + 1)} {q : α → Prop}
+(st : Player → Set (Subtype q))
+(hst : st ∈ {s: Player → Set (Subtype q)|∀ p, Small.{u} (s p)}) : (λ p => Set.image Subtype.val (st p)) ∈ {s: Player→Set α| ∀ p, Small.{u} (s p)} := by 
+  simp only [Player.forall, Set.mem_setOf_eq]
+  simp only [Player.forall, Set.mem_setOf_eq] at hst
+  let ⟨b,d⟩ := hst
+  constructor
+  · exact subtype_set_small (q) (st Player.left)
+  · exact subtype_set_small (q) (st Player.right)
 
--- -- the following is from ChatGPT (unfortunately)  
--- theorem acc_all {A : Type} (x : Primordial A) : Acc option x := by 
---   apply (QPF.Fix.ind)
---   rintro val ⟨fx,rfl⟩
---   cases fx with 
---   | inl a =>
---     constructor
---     intro y h1
---     rcases h1 with ⟨hcomp, hy⟩
---     cases hcomp
---   | inr g =>
---     let ⟨st,hst⟩ := g;
---     constructor
---     rintro y hy
---     rw [map_def] at hy
---     dsimp only [option]at hy
---     simp only [Set.mem_iUnion] at hy
---     have hpull:  PrimordialFunctor.map A (Subtype.val) (Sum.inr ⟨st, hst⟩) = @Sum.inr A (_) (⟨(λ p => Set.image Subtype.val (st p)),tempName st hst⟩) := by congr;
---     rw [hpull] at hy
---     dsimp [moves,moves_or] at hy
---     simp only [QPF.Fix.dest_mk] at hy 
---     rcases hy with ⟨a,b,⟨c,hc⟩,⟨dl,dr⟩⟩
---     simp only at dr
---     rw [dr] at hc
---     exact hc
+theorem acc_all {A : Type} (x : Primordial A) : Acc option x := by 
+  apply (QPF.Fix.ind)
+  rintro val ⟨fx,rfl⟩
+  cases fx with 
+  | inl a =>
+    constructor
+    intro y h1
+    rcases h1 with ⟨hcomp, hy⟩
+    cases hcomp
+  | inr g =>
+    let ⟨st,hst⟩ := g;
+    constructor
+    rintro y hy
+    rw [PrimordialFunctor.map_def] at hy
+    dsimp only [option]at hy
+    simp only [Set.mem_iUnion] at hy
+    have hpull:  PrimordialFunctor.map A (Subtype.val) (Sum.inr ⟨st, hst⟩) = @Sum.inr A (_) (⟨(λ p => Set.image Subtype.val (st p)),tempName st hst⟩) := by congr;
+    rw [hpull] at hy
+    dsimp [moves,moves_or] at hy
+    simp only [QPF.Fix.dest_mk] at hy 
+    rcases hy with ⟨a,b,⟨c,hc⟩,⟨dl,dr⟩⟩
+    simp only at dr
+    rw [dr] at hc
+    exact hc
 
--- --  rw [← Sum.getRight_inr] at hg
--- theorem subposition_wf {A : Type} : @WellFounded (Primordial A) Subposition := by
---   refine ⟨fun x => Acc.transGen ?x⟩
---   exact acc_all x
+
+theorem subposition_wf {A : Type} : @WellFounded (Primordial A) Subposition := by
+  refine ⟨fun x => Acc.transGen ?x⟩
+  exact acc_all x
 
 -- instance {A : Type} : IsWellFounded (Primordial A) Subposition := ⟨subposition_wf⟩
 -- instance {A : Type} : WellFoundedRelation (Primordial A) := ⟨Subposition, instIsWellFoundedSubposition.wf⟩
